fbf49158f0d680255ef1fba4b868be816d5b2ea9
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-12-17 21:49:41 +0530

    chore: adding ruff formatting

--- feluda/feluda.py
+++ feluda/feluda.py
@@ -7,12 +7,14 @@ log = logging.getLogger(__name__)
 # test change in feluda for github aciton
 # change 7
 
+
 class Feluda:
     def __init__(self, configPath):
         self.config = config.load(configPath)
         self.store = None
         if self.config.operators:
             from feluda.operator import Operator
+
             self.operators = Operator(self.config.operators)
 
     def setup(self):
