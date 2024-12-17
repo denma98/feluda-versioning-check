a0a655fe1aa025ad8f8629cbd6b9fbaff59feb8f
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-12-17 11:34:45 +0530

    feat: project setup

---
+++ feluda/feluda.py
@@ -0,0 +1,19 @@
+import logging
+from feluda import config
+from enum import Enum
+
+log = logging.getLogger(__name__)
+
+# test change in feluda for github aciton
+
+class Feluda:
+    def __init__(self, configPath):
+        self.config = config.load(configPath)
+        self.store = None
+        if self.config.operators:
+            from feluda.operator import Operator
+            self.operators = Operator(self.config.operators)
+
+    def setup(self):
+        if self.operators:
+            self.operators.setup()
