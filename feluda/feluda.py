68531f39a5d1c5058c8c20dfa429eb496d8d4752
priyash7 <priyashshah4@gmail.com>
2024-12-19 18:18:05 +0530

    fix: release check

--- feluda/feluda.py
+++ feluda/feluda.py
@@ -5,7 +5,7 @@ from feluda import config
 log = logging.getLogger(__name__)
 
 # test change in feluda for github aciton
-# change 31
+# change 33
 
 class Feluda:
     def __init__(self, configPath):
