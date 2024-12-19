7ee17f261231e5d5a7bc0145c9eaa320a69c6930
priyash7 <priyashshah4@gmail.com>
2024-12-19 17:31:09 +0530

    fix: comments fix

--- feluda/feluda.py
+++ feluda/feluda.py
@@ -5,7 +5,7 @@ from feluda import config
 log = logging.getLogger(__name__)
 
 # test change in feluda for github aciton
-# change 28
+# change 29
 
 class Feluda:
     def __init__(self, configPath):
