5e20dbf585013179a6a373a033f35594f9660ab7
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-12-20 18:06:00 +0530

    feat: update feluda

--- feluda/feluda.py
+++ feluda/feluda.py
@@ -5,7 +5,7 @@ from feluda import config
 log = logging.getLogger(__name__)
 
 # test change in feluda for github aciton
-# change 39
+# change 40
 
 class Feluda:
     def __init__(self, configPath):
