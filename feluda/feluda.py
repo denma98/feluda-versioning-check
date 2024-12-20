23e4a2837fd0a9136687fd89329bc66f0a697de9
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-12-20 13:27:03 +0530

    feat: changes to both packages with feat (#42)

--- feluda/feluda.py
+++ feluda/feluda.py
@@ -5,7 +5,7 @@ from feluda import config
 log = logging.getLogger(__name__)
 
 # test change in feluda for github aciton
-# change 34
+# change 35
 
 class Feluda:
     def __init__(self, configPath):
